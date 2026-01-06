a1c86a02712b88164a0a608df77cfffa2140e990
solomon54 <solomontsehay54@gmail.com>
2026-01-06 13:08:44 +0300

    implementing mobile compnents in react native

---
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -0,0 +1,41 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.continor}>
+      <Text>Enter Screen - Awesome</Text>
+      <Text style={styles.largeText}>
+        TypeScript is great if you practice more
+      </Text>
+      <Text style={styles.mediumText}>
+        React Native provides you a single codebase for cross platforms
+      </Text>
+      <Text style={styles.smallText}>ALX is awesome</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  continor: {
+    backgroundColor: "#90caf9",
+  },
+  largeText: {
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: 700,
+    fontVariant: ["small-caps"],
+  },
+  mediumText: {
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
+  },
+  smallText: {
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
+  },
+});
