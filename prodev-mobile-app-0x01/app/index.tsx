3059f48743d9bb7a0ea7b1d19f2972bb3d43b992
solomon54 <solomontsehay54@gmail.com>
2026-01-06 13:09:55 +0300

    implementing mobile compnents in react native

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
     <View style={styles.continor}>
       <Text>Enter Screen - Awesome</Text>
